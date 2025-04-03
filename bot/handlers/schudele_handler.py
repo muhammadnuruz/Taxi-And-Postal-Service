434dc9e71f23b191ba87cc716479e3316effc239
muhammadnur <muhammadnurpdp@gmail.com>
2025-04-03 22:16:04 +0500

    first commit

--- bot/handlers/schudele_handler.py
+++ bot/handlers/schudele_handler.py
@@ -1,17 +1,24 @@
-import asyncio
 import logging
-from aiogram import Bot
+import asyncio
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram import Bot
 
-async def send_periodic_messages(bot: Bot, group_ids: list, message_text: str, button_text: str, button_url: str, interval: int = 600):
-    keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton(text=button_text, url=button_url))
+
+
+async def send_periodic_messages(bot: Bot):
+    GROUP_IDS = [-1002609258755, -1002646296317, -1002407377689]
+    MESSAGE_TEXT = "📍 ИШОНЧЛИ ХАЛОЛ ТАКСИ ХИЗМАТИДАН ФОЙДАЛАНИНГ👇\n@Toshkent_Samarqand_Taksi_Uz_Bot"
+    BUTTON_TEXT = "📌 ИШОНЧЛИ ХАЛОЛ ТАКСИ ХИЗМАТИ"
+    BUTTON_URL = "https://t.me/Toshkent_Samarqand_Taksi_Uz_Bot"
+
+    keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton(text=BUTTON_TEXT, url=BUTTON_URL))
 
     while True:
-        for group_id in group_ids:
+        for group_id in GROUP_IDS:
             try:
-                await bot.send_message(chat_id=group_id, text=message_text, reply_markup=keyboard)
+                await bot.send_message(chat_id=group_id, text=MESSAGE_TEXT, reply_markup=keyboard)
                 logging.info(f"✅ Xabar {group_id} guruhga yuborildi.")
             except Exception as e:
                 logging.error(f"❌ Xabar yuborishda xatolik ({group_id}): {e}")
 
-        await asyncio.sleep(interval)
+        await asyncio.sleep(600)
--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 import logging
 import asyncio
 from aiogram import executor
-
-from bot.dispatcher import bot
-from bot.handlers.schudele_handler import send_periodic_messages
+from bot.dispatcher import bot, dp
+from bot.handlers import send_periodic_messages
 
 admins = [1974800905, 725168806]
-GROUP_IDS = [-1002609258755, -1002646296317, -1002407377689]
-MESSAGE_TEXT = "📍 ИШОНЧЛИ ХАЛОЛ ТАКСИ ХИЗМАТИДАН ФОЙДАЛАНИНГ👇\n@Toshkent_Samarqand_Taksi_Uz_Bot"
-BUTTON_TEXT = "📌 ИШОНЧЛИ ХАЛОЛ ТАКСИ ХИЗМАТИ"
-BUTTON_URL = "https://t.me/Toshkent_Samarqand_Taksi_Uz_Bot"
-
 
 async def on_startup(dp):
-    asyncio.create_task(send_periodic_messages(bot, GROUP_IDS, MESSAGE_TEXT, BUTTON_TEXT, BUTTON_URL))
-
+    asyncio.create_task(send_periodic_messages(bot))
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    from aiogram import Dispatcher
-
-    dp = Dispatcher(bot)
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
